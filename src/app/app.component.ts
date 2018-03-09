7a4bd4f5b1b8ffb7ca555e52c4e0655e1ed8560c
Abraham McIlvaine <abemac@udel.edu>
2018-03-08 19:19:54 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  branding = 'Reagant Management';
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    document.title = this.branding;
+  }
+}
