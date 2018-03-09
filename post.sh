c0f7a43cde5231767d61f75795c25bd06fbd6d35
Abraham McIlvaine <abemac@udel.edu>
2018-03-08 20:26:08 -0500

    don't need these fiels

--- post.sh
+++
@@ -1,3 +0,0 @@
-curl --user admin:Ff2SYr9oYLni --request POST --header "Accept:application/json" --header "Content-Type:application/json" --data \
-"{'grade':'B','name':'citric acid','puritylevel':'.6'}" \
-https://dev17581.service-now.com/api/now/table/x_197846_team_nan_reagent -s | jq -r
