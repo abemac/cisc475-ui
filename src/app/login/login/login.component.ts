1a1a5c95761014b5585af2266ff109cf8dbe3d1d
Abraham McIlvaine <abemac@udel.edu>
2018-03-15 21:14:27 -0400

    finished most of login system

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -10,21 +10,41 @@ export class LoginComponent implements OnInit {
   @Output() loggedIn= new EventEmitter();
   username:any;
   password:any;
-
+  loggingin:boolean =false;
+  shakegroup = ""
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     
   }
   onLoginClick(){
-    this.http.get<any>('/api/now/table/x_197846_global_token').subscribe( res => {
-      var users = res.result.map( user => {
-        return user;
-      });
-      console.log(users)
-      this.loggedIn.emit("loggedin")
-
-    });
+    this.loggingin=true;
+    this.http.get<any>('/api/now/table/x_197846_global_token').toPromise().then(
+      resp=>{
+        var valid=false;
+        for(let user of resp.result){
+          if (this.username == user.username && this.password==user.password){
+            valid=true;
+            break;
+          }
+        }
+       
+        if(!valid){
+          console.log("Incorrect Login")
+          this.loggingin=false;
+          this.shakegroup="shake"
+          this.password=""
+          setTimeout(()=>{this.shakegroup=""},1000)
+        }
+        if(valid){
+          console.log("Logging in "+this.username)
+          this.loggingin=false;
+          this.loggedIn.emit("loggedin")
+          
+        }
+      }
+    )
+  
     
   }
 
